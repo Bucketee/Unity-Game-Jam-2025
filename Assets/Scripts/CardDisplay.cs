364f7ffab7588a2288ba15b0b5dd42e25e8b8860
bucket <bucketyang@gmail.com>
2025-11-08 19:04:58 +0900

    card and hand
    card and hand

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -1,15 +1,19 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardDisplay : MonoBehaviour
+public class CardDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Card card;
 
     private TextMeshProUGUI cardNameText;
     private Image cardImage;
     private TextMeshProUGUI cardTextText;
+
+    private int originSiblingIndex;
 
     public void Awake()
     {
@@ -26,5 +30,52 @@ public class CardDisplay : MonoBehaviour
         cardNameText.text = card.cardName;
         cardImage.sprite = card.cardImage;
         cardTextText.text = card.cardText;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ToggleCard();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        UnToggleCard();
+    }
+
+    private void ToggleCard()
+    {
+        transform.parent.GetComponent<Hand>().OrganizeCards();
+        originSiblingIndex = transform.GetSiblingIndex();
+
+        StartCoroutine(ToggleCo());
+    }
+
+    private IEnumerator ToggleCo()
+    {
+        float duration = 0.2f;
+        float time = 0;
+
+        transform.rotation = Quaternion.identity;
+        
+        while (time < duration)
+        {
+            Debug.Log(Time.deltaTime);
+            time += Time.deltaTime;
+            transform.localPosition = Vector3.Lerp(new Vector3(180 * (transform.GetSiblingIndex() -
+                                                                      ((float)(transform.parent.childCount - 1) / 2)), 10, 0), new Vector3(210 * (transform.GetSiblingIndex() -
+                ((float)(transform.parent.childCount - 1) / 2)), 30, 0), time / duration);
+            yield return null;
+        }
+
+        transform.localPosition = new Vector3(210 * (transform.GetSiblingIndex() - ((float)(transform.parent.childCount - 1) / 2)), 30, 0);
+        transform.SetAsLastSibling();
+    }
+
+    private void UnToggleCard()
+    {
+        StopAllCoroutines();
+        
+        transform.SetSiblingIndex(originSiblingIndex);
+        transform.parent.GetComponent<Hand>().OrganizeCards();
     }
 }
